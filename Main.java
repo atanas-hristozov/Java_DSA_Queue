7204a30f2cec3b995c242751974d39d46964cfc5
atanas-hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-08-27 10:53:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+        System.out.println(queue.isEmpty());
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
+        queue.offer("Harold");
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.size());
+        System.out.println(queue.contains("Harold"));
+        System.out.println(queue);
+        System.out.println(queue.peek());
+        queue.poll();
+        queue.poll();
+        System.out.println(queue);
+    }
+}
